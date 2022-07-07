b093e5ef36b25ea252465638eda0338c3a008111
mohitsi2 <mohitsi2@cisco.com>
2022-07-07 17:40:24 +0530

    Initial backend commit

---
+++ backend/src/test/java/net/penguincoders/budgetplanner/BudgetPlannerApplicationTests.java
@@ -0,0 +1,16 @@
+package net.penguincoders.budgetplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BudgetPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
