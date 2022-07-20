56f3af555400973847918f8c15a2f075b8f02739
mohitsi2 <mohitsi2@cisco.com>
2022-07-20 22:38:38 +0530

    Added MongoAuditing to fix CreatedAt UpdatedAt in MongoDB

--- backend/src/main/java/net/penguincoders/budgetplanner/BudgetPlannerApplication.java
+++ backend/src/main/java/net/penguincoders/budgetplanner/BudgetPlannerApplication.java
@@ -2,8 +2,10 @@ package net.penguincoders.budgetplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class BudgetPlannerApplication {
 
     public static void main(String[] args) {
