6468db8b41e3863900ee45e1606607d6d88308c3
mohitsi2 <mohitsi2@cisco.com>
2022-07-20 21:33:41 +0530

    Added Login/Signup Functionality

--- frontend/src/app/components/login/login.module.ts
+++ frontend/src/app/components/login/login.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule, RouterModule],
   exports: [LoginComponent],
 })
 export class LoginModule {}
--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthControllerService } from 'src/openapi-generated';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  signupForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private toast: HotToastService,
+    private authService: AuthControllerService,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  signup() {
+    const signupData = this.signupForm.value;
+    this.authService
+      .registerUser(signupData)
+      .pipe(
+        this.toast.observe({
+          success: 'Registered successfully',
+          loading: 'Registering...',
+          error: ({ message }) =>
+            `There was an error signing you in: ${message} `,
+        }),
+      )
+      .subscribe(() => {
+        this.router.navigate(['/tabs/home']);
+      });
+  }
 }
