35071530fb79767fb65215bed739c4c1956b2ab4
mohitsi2 <mohitsi2@cisco.com>
2022-07-21 18:48:16 +0530

    Add Auth Guard and Login Guard to routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './utils/auth.guard';
+import { LoginGuard } from './utils/login.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'auth',
     loadChildren: () =>
       import('./pages/auth/auth.module').then((m) => m.AuthPageModule),
+    canActivate: [LoginGuard],
   },
   {
     path: '**',
--- frontend/src/app/tab3/tab3.page.ts
+++ frontend/src/app/tab3/tab3.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() {}
-
+  logout() {
+    this.authService.logout();
+  }
 }
