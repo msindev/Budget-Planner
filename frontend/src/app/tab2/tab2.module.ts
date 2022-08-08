a92c8a4b4ea9d816b671ab45ccdf36cd70f74a55
mohitsi2 <mohitsi2@cisco.com>
2022-08-08 20:41:15 +0530

    Add Expense Form Frontend Implementation

--- frontend/src/app/tab2/tab2.module.ts
+++ frontend/src/app/tab2/tab2.module.ts
@@ -1,14 +1,19 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
-  imports: [IonicModule, CommonModule, FormsModule, Tab2PageRoutingModule],
+  imports: [
+    IonicModule,
+    CommonModule,
+    ReactiveFormsModule,
+    Tab2PageRoutingModule,
+  ],
+  providers: [DatePipe],
   declarations: [Tab2Page],
 })
 export class Tab2PageModule {}
--- frontend/src/app/tab2/tab2.page.ts
+++ frontend/src/app/tab2/tab2.page.ts
@@ -1,12 +1,52 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { ExpenseControllerService } from 'src/openapi-generated';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  expenseForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private expenseService: ExpenseControllerService,
+    private authService: AuthService,
+    private toast: HotToastService,
+    private router: Router,
+    private datePipe: DatePipe,
+  ) {
+    this.expenseForm = this.fb.group({
+      name: ['', Validators.required],
+      amount: ['', Validators.required],
+      date: ['', Validators.required],
+      category: ['', Validators.required],
+      description: [''],
+    });
+  }
 
-  constructor() {}
-
+  onSubmit() {
+    console.log(this.expenseForm.value);
+    const username = this.authService.getUsername();
+    let expense = { ...this.expenseForm.value };
+    expense.date = this.datePipe.transform(expense.date, 'yyyy-MM-dd');
+    this.expenseService
+      .addExpense(username, expense)
+      .pipe(
+        this.toast.observe({
+          success: 'Expense Saved successfully',
+          loading: 'Saving Expense...',
+          error: () => `There was an error saving your expense `,
+        }),
+      )
+      .subscribe(() => {
+        this.expenseForm.reset();
+        this.router.navigate(['/tabs/home']);
+      });
+  }
 }
